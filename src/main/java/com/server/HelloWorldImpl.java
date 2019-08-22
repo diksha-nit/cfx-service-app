09b699d8966c2adcb8b0f352f82d405d63a3f0cf
train <train@TRN5470.dmztrn.amdocs.trn>
2019-08-22 09:37:42 +0530

    commit2 :reverted the change

--- src/main/java/com/server/HelloWorldImpl.java
+++ src/main/java/com/server/HelloWorldImpl.java
@@ -14,7 +14,7 @@ public class HelloWorldImpl implements HelloWorld {
 
     public String sayHi(String text) {
         System.out.println("sayHi called");
-        return "Welcome " + text;
+        return "Hello" + text;
     }
 
     public String sayHiToUser(User user) {
